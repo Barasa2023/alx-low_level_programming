fdbac9d0d01788a1ed94181155e1b2f6295c4b41
Kelvin Barasa <officialbarasa@gmail.com>
2023-02-16 05:07:22 -0800

    C is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main - function to print a line of characters
+ *
+ * return: return 0 on success
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is building a multilingual puzzle \n");
+
+	return 0;
+}
