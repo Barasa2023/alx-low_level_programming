ea6aa2f8c4377533ff3d01e6d75a3f8acab19ce2
Kelvin Barasa <officialbarasa@gmail.com>
2023-02-23 08:03:16 -0800

    First task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * int _isupper - checks whether aa character is uppercase
+ * @c: character
+ *
+ * return: 0
+ */
+int _isupper(int c)
+{
+	int c;
+
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
