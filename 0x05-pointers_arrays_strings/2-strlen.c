eb3b9255e544c5331a2fd7870e30bb7e6e37c314
Kelvin Barasa <officialbarasa@gmail.com>
2023-02-27 01:15:21 -0800

    Lentgh of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - calculate the length of a string
+ * @s: string to find the length
+ *
+ * return: 0
+ */
+int _strlen(char *s)
+{
+	size_t strlen(char *s);
+
+	return (0);
+}
