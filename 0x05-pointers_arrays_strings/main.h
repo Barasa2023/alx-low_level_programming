cb62bcc1374d283d88b4d93b4f41f10cb2cb2a01
Kelvin Barasa <officialbarasa@gmail.com>
2023-02-27 00:38:36 -0800

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef "MAIN.H"
-#define "main.h"
+#ifndef MAIN.H
+#define MAIN.H
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -8,4 +8,4 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 
-#endif
+#endif /* MAIN.H */
