34ca3cd3e7b830b7ec63e154f25a025b38a6782d
Kelvin Barasa <officialbarasa@gmail.com>
2023-02-27 00:51:42 -0800

    Update files

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,16 +3,11 @@
 
 /**
  * reset_to_98 - updates the value stored at *n
- * n: value to be updated
+ * @n: value to be updated
  *
  * Return: Always return 0 on success
  */
 void reset_to_98(int *n)
 {
-	int *n;
-
 	*n = 98;
-	printf("%d\n", *n);
-
-	return (0);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -8,4 +5,3 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 
-#endif /* MAIN_H */
